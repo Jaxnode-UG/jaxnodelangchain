aa697fca4846915343ecac00a508a2485b5d5ad1
David Fekke <david@fekke.com>
2024-03-10 20:40:20 -0400

    Pre-checkin before merge of HWSWLib

--- example.js
+++ example.js
@@ -1,18 +1,22 @@
 // Wite a Langchain js program that sends a prompt to the OpenAI LLM model and outputs the results to a stream in the process.stdout.
+// Also use the dotenv module to load the API key from the .env file.
+// We are using package.json type module to import the dotenv module.
 
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 dotenv.config();
-import { ChatOpenAI } from "@langchain/openai";
-import { ChatPromptTemplate } from "@langchain/core/prompts";
-import { StringOutputParser } from "@langchain/core/output_parsers";
+
+import { ChatOpenAI } from '@langchain/openai';
+import { ChatPromptTemplate } from '@langchain/core/prompts';
+import { StringOutputParser } from '@langchain/core/output_parsers';
 
 const prompt = ChatPromptTemplate.fromMessages([
-    ["human", "What is the formula for converting Celsius to Fahrenheit temperature"]
+    ["human", "What is the formula for converting Fahrenheit to Celsius?"]
 ]);
-const model = new ChatOpenAI({ temperature: 0 });
+const model = new ChatOpenAI();
 const outputParser = new StringOutputParser();
 
 const chain = prompt.pipe(model).pipe(outputParser);
+
 const stream = await chain.stream();
 
 for await (const chunk of stream) {
